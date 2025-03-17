d17620f0b846b0e5cb96907e9ed2b7dd92886fd4
kishore-rao-k <kishoreraok12@gmail.com>
2025-03-17 20:12:25 +0530

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
 
 const App = () => {
   return (
-    <div className='bg-red-200 p-4'>App</div>
-  )
-}
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
 
-export default App
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default App;
