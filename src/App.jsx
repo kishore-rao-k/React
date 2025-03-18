c6be977d4542685709dcd5bb987656939f7afe27
kishore-rao-k <kishoreraok12@gmail.com>
2025-03-18 11:36:25 +0530

    updated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Route, createBrowserRouter, createRoutesFromElements ,RouterProvider  } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element ={<h1>My App</h1>}/>)
+)
 const App = () => {
   return (
     <>
@@ -12,14 +17,7 @@ const App = () => {
       <HomeCards />
       <JobListings />
 
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+     <ViewAllJobs/>
     </>
   );
 };
