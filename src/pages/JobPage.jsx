fdafa7726ee419796134f5dc17c522a7429482e4
kishore-rao-k <kishoreraok12@gmail.com>
2025-03-19 23:05:48 +0530

    updated edit job page

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { useParams, useLoaderData, useNavigate} from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
+import { ToastContainer } from 'react-toastify'
 const JobPage = ({deleteJob}) =>{
     const navigate = useNavigate();
     const {id} = useParams();
@@ -13,6 +16,12 @@ const JobPage = ({deleteJob}) =>{
             return;
         }
         deleteJob(jobId)
+       
+        toast.success('Job deleted successfully', {
+            position: 'top-right',
+            autoClose: 3000, 
+        });
+
         navigate('/jobs')
     }
     // const [job,setJob] = useState(null);
@@ -95,7 +104,7 @@ const JobPage = ({deleteJob}) =>{
                   <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                     <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                     <Link
-                      to={`/jobs/edit/${job.id}`}
+                      to={`/edit-job/${job.id}`}
                       className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                     >
                       Edit Job
